535db8de4723b96ad68a7edffbb41efcbe978dc3
Karim <karim_kotb@outlook.com>
2026-05-04 03:29:58 +0300

    fixed cors error

--- nodesmartsecure/Src/oop_server.js
+++ nodesmartsecure/Src/oop_server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://smart-secure-ten.vercel.app/"],
+    origin: ["http://localhost:3000", "https://smart-secure-ten.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
